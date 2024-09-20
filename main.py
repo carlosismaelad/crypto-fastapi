e124bcc717e68f90053618eb53a8ac960474bede
Carlos Dourado <carlosismaelad@outlook.com>
2024-09-20 01:15:40 -0300

    Add user_router

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+from controllers.user_controller import user_router
 import uvicorn
 
 app = FastAPI()
@@ -9,6 +10,7 @@ def first():
     return 'Hello from FastAPI!'
 
 app.include_router(prefix='/home', router=router)
+app.include_router(user_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
